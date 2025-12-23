0ec3bf001a227a5b7380eb0599e7a05c6357badc
Mirion <51433180+mirion-dev@users.noreply.github.com>
2025-12-23 11:50:33 +0800

    remove ui.h which is no longer needed

--- Polymer/ui.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <Windows.h>
-
-LRESULT ImGui_ImplWin32_WndProcHandler(HWND, UINT, WPARAM, LPARAM);
--- Polymer/ui.ixx
+++ Polymer/ui.ixx
@@ -1,7 +1,5 @@
 module;
 
-#include "ui.h"
-
 #include <d3d9.h>
 #include <imgui_impl_win32.h>
 #include <imgui_impl_dx9.h>
